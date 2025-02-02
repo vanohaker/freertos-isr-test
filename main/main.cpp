c6a2d3251c65edba8a996053bdfb62e6754e7af4
Ivan <vanohaker@mail.ru>
2025-02-02 22:00:45 +0300

    srgferg

--- main/main.cpp
+++ main/main.cpp
@@ -9,16 +9,17 @@ static QueueHandle_t gpio_btn_queue = NULL;
 void gpio_pin_init() {
     // Пустая конфигурация GPIO
     gpio_config_t io_conf = {};
-    // Разрешаем прерывания по спаду. Тоесть мы должны подать минус на пин чтобы сработало прерывание 
-    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    // Разрешаем прерывания по спаду.
+    io_conf.intr_type = GPIO_INTR_NEGEDGE;
     // Битовая маска пина к которому применяются настройки
     // 1ULL<<GPIO_NUM_14 == 0000000000000000000000000100000000000000
     io_conf.pin_bit_mask = (1ULL<<GPIO_NUM_14);
     // В какую сторону будетработать пин (вход или выход)
     // Кнопка работает на вход
     io_conf.mode = GPIO_MODE_INPUT;
-    // Включаем подтягивающий резистор потому что кнопка подключена к GND
-    io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    // Подтяжка к верху
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
     // Устанавливаем конфигурацию
     ESP_ERROR_CHECK(gpio_config(&io_conf));
 
